99eaf9c3b7fbd60abcfb8146a8d02fb2fd534a58
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-02-07 20:57:50 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from './Page/LandingPage'
+
+const App = () => {
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/Page/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const LandingPage = () => {
+    return (
+        <>
+            <Header />
+            landing
+        </>
+    )
+}
+
+export default LandingPage
