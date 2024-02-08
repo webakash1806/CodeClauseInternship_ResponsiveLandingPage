a4fcb29e344d2398ae7bde07d07d618cfd78b612
Akash Kumar Singh <itsakash18.06@gmail.com>
2024-02-08 17:06:57 +0530

    Footer added

--- src/Page/Footer.jsx
+++ src/Page/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
+    const date = new Date().getFullYear()
     return (
-        <div>
-            footer
+        <div className='text-center border-t border-slate-600'>
+            <p className='p-2 text-white text-[1.05rem]'>&#169;<span> {date}</span> Mighty Cart | All rights reserved</p>
         </div>
     )
 }
--- src/Page/LandingPage.jsx
+++ src/Page/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 const LandingPage = () => {
     return (
-        <>
+        <div className='flex flex-col justify-between h-[100vh]'>
             <Header />
             <div>Landing page</div>
-        </>
+            <Footer />
+        </div>
     )
 }
 
